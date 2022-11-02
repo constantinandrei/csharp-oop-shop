38ffb76685f63fb2b86717fb9575993faeedb09b
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-02 16:06:10 +0100

    complete exercise with c# pad left builtin function

--- Program.cs
+++ Program.cs
@@ -13,22 +13,47 @@
 //Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
 //Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto.
 //BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
-;
+
+Product product1 = new Product("penna", "per scrivere", 0.78);
+
+Console.WriteLine("prezzo pieno: " + product1.GetFullPrice());
+Console.WriteLine("nome completo: " + product1.GetFullName());
+
+// cambio prezzo e prova
+
+product1.SetPrice(9.87);
+Console.WriteLine("prezzo pieno: " + product1.GetFullPrice());
+
+// cambio iva e prova
+
+product1.SetIva(0.04);
+Console.WriteLine("prezzo pieno: " + product1.GetFullPrice());
+
+// cambio nome e prova
+
+product1.SetName("postit");
+Console.WriteLine("nome completo: " + product1.GetFullName());
+
+// cambio descrizione e prova
+
+product1.SetDescription("per scriverci sopra");
+Console.WriteLine(product1.GetDescription());
+
 public class Product
 {
     int id;
     string name;
     string description;
-    float price;
-    float iva;
+    double price;
+    double iva;
 
-    public Product(string name, string description, float price)
+    public Product(string name, string description, double price)
     {
         this.name = name;
         this.description = description;
         this.price = price;
-        id = new Random().Next(1, 100001);
-        iva = 0.22f;
+        id = new Random().Next(1, 10001);
+        iva = 0.22;
     }
 
     public int GetId()
@@ -56,23 +81,35 @@ public class Product
         this.description = description;
     }
 
-    public float GetPrice()
+    public double GetPrice()
     {
         return price;
     }
 
-    public void SetPrice (float price)
+    public void SetPrice (double price)
     {
         this.price = price;
     }
 
-    public float GetIva()
+    public double GetIva()
     {
         return iva;
     }
 
-    public void SetIva(float iva)
+    public void SetIva(double iva)
     {
         this.iva = iva;
+    }
+
+    // metodo che restituisce prezzo + iva
+    public double GetFullPrice()
+    {
+        return Math.Round((this.price + this.price * iva), 2);
+    }
+
+    public string GetFullName()
+    {
+        string idString = this.id.ToString();
+        return this.name + "-" + idString.PadLeft(8, '0');
     }
 }
